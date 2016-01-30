50231c6e4c9cadf75bf4b63f37b8b13eefd7afe8
garicchi <xgaricchi@gmail.com>
2016-01-31 03:08:13 +0900

    todoは形に

---
+++ 3/api.php
@@ -0,0 +1,32 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    http_response_code(200);
+    if(isset($_COOKIE['tasks'])){
+        $json = $_COOKIE['tasks'];
+        print($json);
+    }else{
+        print('{}');
+    }
+}else if($_SERVER["REQUEST_METHOD"] == "POST"){
+    http_response_code(200);
+    
+    $task = $_POST['task'];
+    $task_array = array();
+    if(isset($_COOKIE['tasks'])){
+        $json = $_COOKIE['tasks'];
+        $task_array = json_decode($json);
+    }
+    
+    array_push($task_array,$task);
+    setcookie('tasks',json_encode($task_array));
+    print('{"message":"success"}');
+}else {
+    http_response_code(400); 
+}
+
+
+?>
