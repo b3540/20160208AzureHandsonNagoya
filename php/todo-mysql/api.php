f5ff635a3386980d4d3744936f73987bc8ecf7e6
garicchi <xgaricchi@gmail.com>
2016-02-24 22:18:55 +0900

    mysql insert完成

--- php/todo-mysql/api.php
+++ php/todo-mysql/api.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
-print('moge');
+
 $sql = mysql_connect('ja-cdbr-azure-west-a.cloudapp.net', 'b82cee9bd25c06', 'ee67f77a');
-print($sql);
+
 $todoDb = mysql_select_db('garitodo',$sql);
 mysql_set_charset('utf-8');
 
@@ -21,7 +21,8 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     $task = $_POST['task'];
     $name = $task['task_name'];
     $date = $task['task_date'];
-    $result = mysql_query('insert into todo (name,date) values ('.$name.','.$date.')');
+    $result = mysql_query('insert into todo(name,date) values ("'.$name.'","'.$date.'")');
+    //print(mysql_error($sql));
     /*
     $task_array = array();
     if(isset($_COOKIE['tasks'])){
@@ -32,7 +33,7 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     array_push($task_array,$task);
     setcookie('tasks',json_encode($task_array));
     */
-    print('{"message":"'.$result.'"}');
+    print('{"message":"'.mysql_error($sql).'"}');
 }else {
     http_response_code(400); 
 }
